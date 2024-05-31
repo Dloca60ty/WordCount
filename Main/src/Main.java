af88c0d08f8fc408097cd361c4edcf6e970ff753
Dloca60ty <166288063+Dloca60ty@users.noreply.github.com>
2024-05-31 20:10:53 +0800

    Add files via upload

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -9,30 +9,30 @@ class Main {
         int wordCount = 0;
         boolean inWord = false;
 
-        try (BufferedReader br = new BufferedReader(new FileReader("src/202131060732.txt"))) {
-            int c;
-            while ((c = br.read()) != -1) {
-                charCount++;
+        BufferedReader br = new BufferedReader(new FileReader("src/202131060732.txt"));
+        int c;
+        while ((c = br.read()) != -1) {
+            charCount++;
 
-                if (Character.isLetter((char) c) || c == '\'') {
-                    inWord = true;
-                } else {
-                    if (inWord) {
-                        wordCount++;
-                        inWord = false;
-                    }
+            if (Character.isLetter((char) c) || c == '\'') {
+                inWord = true;
+            } else {
+                if (inWord) {
+                    wordCount++;
+                    inWord = false;
                 }
             }
-
-            // 判断是不是最后一个单词
-            if (inWord) {
-                wordCount++;
-            }
-
-            System.out.println("字符数: " + charCount);
-            System.out.println("单词数: " + wordCount);
-
         }
+
+        // 判断是不是最后一个单词
+        if (inWord) {
+            wordCount++;
+        }
+
+        System.out.println("字符数: " + charCount);
+        System.out.println("单词数: " + wordCount);
+
+
     }
 }
 
